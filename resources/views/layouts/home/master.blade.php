029adbc726a62b8101c00265fb923df1c003d391
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-08-01 18:46:22 -0300

    Adicionado rodapé do site.

--- resources/views/layouts/home/master.blade.php
+++ resources/views/layouts/home/master.blade.php
@@ -57,6 +57,54 @@
     </main>
 
     <footer class="page-footer">
+        <div class="bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <ul class="no-style">
+                            <li><a class="white" href="https://www.suafigurinha.com.br/lbuns-colecion-veis">Álbuns Colecionáveis</a></li>
+                            <li><a class="white" href="https://www.suafigurinha.com.br/albuns-personalizados-1">Álbuns Personalizados</a></li>
+                            <li><a class="white" href="https://www.suafigurinha.com.br/foto-produtos-1">Foto Produtos</a></li>
+                            <li><a class="white" href="https://www.suafigurinha.com.br/shipping-and-returns">Políticas da Loja</a></li>
+                            <li><a class="white" href="https://www.suafigurinha.com.br/como-enviar-as-fotos">Como enviar as fotos</a></li>
+                            <li><a class="white" href="https://www.suafigurinha.com.br/duvidas-frequentes">Dúvidas Frequentes</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm">
+                        <ul class="no-style">
+                            <li class="title">Sua Figurinha</li>
+                            <li class="white">CNPJ: 37.650.106/0001-40</li>
+                            <li class="white">
+                                <span style="font-size: 24px;">
+                                    <i class="fab fa-whatsapp"></i>
+                                </span> 
+                                <a class="white" href="tel:+551632355960">(16) 3235-5960</a>
+                            </li>
+                            <li class="white">
+                                <span style="font-size: 24px;">
+                                    <i class="fas fa-user-friends"></i>
+                                </span> 
+                                8h às 18h de segunda a sexta
+                            </li>
+                            <li class="white">
+                                <span style="font-size: 24px;">
+                                    <i class="far fa-envelope"></i>
+                                </span> 
+                                <a class="white" href="mailto:suafigurinha@gmail.com">suafigurinha@gmail.com</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-sm">
+                        <a style="cursor: pointer; width: 234px; height: 137px;" href="https://pagseguro.uol.com.br/sobre/#rmcl" target="_blank" data-content="https://pagseguro.uol.com.br/sobre/#rmcl" data-type="external" rel="noopener" id="comp-jzlczuwalink" class="ca1link"><wix-image style="width:234px;height:137px;top:0;left:0" data-has-bg-scroll-effect="" data-image-info="{&quot;imageData&quot;:{&quot;type&quot;:&quot;Image&quot;,&quot;id&quot;:&quot;dataItem-jzlczv3y&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;c1dmp&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;2.0&quot;,&quot;isHidden&quot;:false},&quot;title&quot;:&quot;&quot;,&quot;uri&quot;:&quot;62209d_2721279ac9bf4419a85b7bfaf622f635~mv2.png&quot;,&quot;description&quot;:&quot;&quot;,&quot;width&quot;:430,&quot;height&quot;:254,&quot;alt&quot;:&quot;pagseguro-2.png&quot;,&quot;name&quot;:&quot;pagseguro-2.png&quot;,&quot;link&quot;:{&quot;type&quot;:&quot;ExternalLink&quot;,&quot;id&quot;:&quot;dataItem-jzoixk9e&quot;,&quot;metaData&quot;:{&quot;pageId&quot;:&quot;c1dmp&quot;,&quot;isPreset&quot;:false,&quot;schemaVersion&quot;:&quot;1.0&quot;,&quot;isHidden&quot;:false},&quot;url&quot;:&quot;https://pagseguro.uol.com.br/sobre/#rmcl&quot;,&quot;target&quot;:&quot;_blank&quot;},&quot;quality&quot;:{&quot;unsharpMask&quot;:{&quot;radius&quot;:1.2,&quot;amount&quot;:1,&quot;threshold&quot;:0.01}},&quot;displayMode&quot;:&quot;fill&quot;},&quot;containerId&quot;:&quot;comp-jzlczuwa&quot;,&quot;displayMode&quot;:&quot;fill&quot;}" data-has-ssr-src="" data-is-svg="false" data-is-svg-mask="false" id="comp-jzlczuwaimg" class="ca1img" data-src="https://static.wixstatic.com/media/62209d_2721279ac9bf4419a85b7bfaf622f635~mv2.png/v1/fill/w_234,h_137,al_c,q_85,usm_1.20_1.00_0.01/pagseguro-2.webp"><img id="comp-jzlczuwaimgimage" style="object-position:50% 50%;width:234px;height:137px;object-fit:cover" alt="pagseguro-2.png" data-type="image" itemprop="image" src="https://static.wixstatic.com/media/62209d_2721279ac9bf4419a85b7bfaf622f635~mv2.png/v1/fill/w_234,h_137,al_c,q_85,usm_1.20_1.00_0.01/pagseguro-2.webp"></wix-image></a>
+                    </div>
+                </div>
+                <div class="social-medias">
+                    <a href="https://www.instagram.com/suafigurinha/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/suafigurinha/"><i class="fab fa-facebook"></i></a>
+                </div>
+            </div>
+        </div>
+        <p class="text-center">© 2020 Colecionadores de Momentos</p>
     </footer>
 
     <div id="toast-container" aria-live="polite" aria-atomic="true"></div>
