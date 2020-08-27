96705ffbddcd60c267f0a9806ea21e159450de8c
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-08-26 21:37:02 -0300

    Realizado alteração de layout.

--- resources/views/home/my-album/success.blade.php
+++ resources/views/home/my-album/success.blade.php
@@ -2,8 +2,8 @@
 @section('Titulo', 'Sua Figurinha - Meu Album')
 @section('content')
     <div class="container">
-        <div class="jumbotron">
-            <h1 class="display-4">Sucesso!!!</h1>
+        <div class="jumbotron text-center">
+            <h1 class="display-4 text-success">Sucesso!!!</h1>
             <p class="lead">Recebemos seu album.</p>
         </div>
     </div>
